2080debcfdf358e7868f6d19821263e04ab58042
Brayan Ocampo <mrx.source.mrx@gmail.com>
2024-10-20 13:35:46 -0500

    change some things

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { NavBar } from "./Components/GlobalComponents/NavBar/NavBar";
-//import { ColorScheme } from "./Components/StyleComponents/ColorScheme/ColorScheme";
 
 createRoot(document.getElementById("root")).render(
-  //<ColorScheme />
   <StrictMode>
     <NavBar />
   </StrictMode>,
