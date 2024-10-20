ddd594a7731a02bfdfd8c674f7f1630f57ba1244
Brayan Ocampo <mrx.source.mrx@gmail.com>
2024-10-20 13:34:54 -0500

    add config for css modules and syntax jsx

--- panda.config.mjs
+++ panda.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from "@pandacss/dev";
 
 export default defineConfig({
+  // css 'modules'
+  syntax: "template-literal",
+  jsxFramework: "react",
+
   // Whether to use css reset
   preflight: true,
 
