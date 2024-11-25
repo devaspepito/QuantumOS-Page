d8172f968f5975c010c97f67d3d97b81694d26a4
Brayan Ocampo <mrx.source.mrx@gmail.com>
2024-11-24 21:14:09 -0500

    pages for route content

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <>
+      <h1>Welcome to QuantumOS</h1>
+      <p>A different ROM based on LineageOS</p>
+    </>
+  )
+}
+
