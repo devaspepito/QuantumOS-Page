109b877359800871b6d7c979f7791e404d01f70a
Brayan Ocampo <mrx.source.mrx@gmail.com>
2024-11-27 11:10:09 -0500

    change background-color

--- src/Components/CssModules/Modules/AnimatedCardArm.jsx
+++ src/Components/CssModules/Modules/AnimatedCardArm.jsx
@@ -3,7 +3,7 @@ import { styled } from "/styled-system/jsx";
 export const DownloadContianer = styled.div`
   height: 440px;
   width: 300px;
-  background-color: beige;
+  background-color: #324b77;
   display: flex;
   flex-direction: column;
   align-items: center;
